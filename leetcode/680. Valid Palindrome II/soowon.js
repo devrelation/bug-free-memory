c37fdf0ea692b792c9ff3ac202b966b2c53f0020
JungSWon <81947118+MiaSWJung@users.noreply.github.com>
2022-04-23 18:08:04 +0900

    [SOLVE] 680 soowon.js

--- leetcode/680. Valid Palindrome II/soowon.js
+++ leetcode/680. Valid Palindrome II/soowon.js
@@ -1,5 +1,12 @@
-const solution = () => {
-  return 1;
+const solution = (s) => {
+  const sLen = s.length;
+  const isOdd = !!(sLen % 2);
+  const quo = ~~(sLen / 2);
+  const fore = s.slice(0, ~~(sLen / 2));
+  const back = isOdd ? s.slice(quo + 1, sLen) : s.slice(quo, sLen);
+  const reversedBack = back.split('').reverse().join('');
+  return fore === reversedBack;
 };
 
 export default solution;
+
