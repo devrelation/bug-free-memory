5e03f88f45f0ea00cf87b4007c4b20cbb5e3f29f
juicyjusung <juicyjusung@gmail.com>
2022-05-02 18:02:01 +0900

    :sparkles: feat: 주성 1249

--- leetcode/1249. Minimum Remove to Make Valid Parentheses/jusung.js
+++ leetcode/1249. Minimum Remove to Make Valid Parentheses/jusung.js
@@ -1,5 +1,13 @@
-const solution = () => {
-  return '';
+const solution = (s) => {
+  let text = s;
+  const regex = /\(([^()]*)\)/g;
+  while (regex.test(text)) {
+    text = text.replace(regex, (_, captured) => {
+      return `[${captured}]`;
+    });
+  }
+
+  return text.replace(/\)|\(/g, '').replaceAll('[', '(').replaceAll(']', ')');
 };
 
 export default solution;
