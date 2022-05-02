216eac2e8b8dc66ffa004a54d5c54bab7637c714
KimDoWoo <kaei2574@gmail.com>
2022-05-02 23:11:26 +0900

    1249 solve
    solve it

--- leetcode/1249. Minimum Remove to Make Valid Parentheses/dowoo.js
+++ leetcode/1249. Minimum Remove to Make Valid Parentheses/dowoo.js
@@ -1,5 +1,21 @@
-const solution = () => {
-  return 1;
-};
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+    let stack = [];
+    let answer = s.split('');
 
-export default solution;
+  for (let i=0; i<s.length; i++) {
+    if (s[i] === '(') stack.push(i);
+    else if (s[i] === ')') {
+      stack.length ? stack.pop() : (answer[i] = '');
+    }
+  }
+    
+  if (stack.length) {
+    while (stack.length) answer[stack.pop()] = '';
+  }
+
+  return answer.join('');
+};
