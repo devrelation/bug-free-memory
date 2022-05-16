29fb0b1c28396ca9042c0aa5186882e6d234eacc
juicyjusung <juicyjusung@gmail.com>
2022-05-16 22:19:29 +0900

    :sparkles: 1644 - jusung

--- leetcode/2022-05-16/1644. Lowest Common Ancestor of a Binary Tree II/jusung.js
+++ leetcode/2022-05-16/1644. Lowest Common Ancestor of a Binary Tree II/jusung.js
@@ -1,5 +1,29 @@
-const solution = () => {
-  return 1;
+const solution = (nums, p, q) => {
+  let pIndex = nums.findIndex((num) => num === p);
+  let qIndex = nums.findIndex((num) => num === q);
+
+  let pDepth = getDepth(pIndex);
+  let qDepth = getDepth(qIndex);
+
+  while (getParent(pIndex) !== getParent(qIndex)) {
+    if (pDepth > qDepth) {
+      pIndex = getParent(pIndex);
+      pDepth--;
+    } else {
+      qIndex = getParent(qIndex);
+      qDepth--;
+    }
+  }
+
+  return pIndex === qIndex ? nums[pIndex] : nums[getParent(pIndex)];
 };
 
 export default solution;
+
+const getDepth = (index) => {
+  return Math.floor(Math.log2(index + 1)) + 1;
+};
+
+const getParent = (index) => {
+  return Math.ceil(index / 2) - 1;
+};
